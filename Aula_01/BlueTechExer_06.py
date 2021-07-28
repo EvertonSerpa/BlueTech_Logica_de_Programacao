57a91ba93d6b071dc1682fa81bf5514185f31763
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-28 15:09:39 -0300

    exer_06

---
+++ Aula_01/BlueTechExer_06.py
@@ -0,0 +1,10 @@
+# 06 - Elabore um programa que recebe dois valores inteiros e mostra se o primeiro valor
+# é maior ou igual ao segundo valor
+print()
+valor01 = int(input('Digite um número: '))
+valor02 = int(input('Digite o segundo número: '))
+print(f'{valor01>valor02}')
+# Explicando o código.
+# foi criado duas variaveis que recebe os valores informado pelo usuario,
+# no print pergunto de o valor01 e > que valor02. se o valor for mais ele vai mostrar True na tela
+# caso contrario ele apresentara False
