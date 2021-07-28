297cb8be6404fdb081460d0e2b8f1bfe674c9a37
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-28 14:46:12 -0300

    exer_05

---
+++ Aula_01/BlueTechExer_05.py
@@ -0,0 +1,9 @@
+# 05 - Elabore um programa que receba 4 notas de um aluno e calcule a média dele.
+print()
+nota01 = float(input('Digite a primeira nota do aluno: '))
+nota02 = float(input('Digite a segunda nota do aluno: '))
+nota03 = float(input('Digite a terceira nota do aluno: '))
+nota04 = float(input('Digite a quarta nota do aluno '))
+media = (nota01 + nota02 + nota03 + nota04) / 4
+print()
+print(f'A primeira nota: {nota01}\nA segunda nota: {nota02}\nA terceira nota: {nota03}\nA quarta nota: {nota04}\nE A média: {media}')
