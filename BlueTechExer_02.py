4aefd338d2444836ff57ccd2fdd63f207d51a866
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-28 11:52:41 -0300

    000

---
+++ BlueTechExer_02.py
@@ -0,0 +1,9 @@
+# 02 - Elabore um programa que escreve seu nome completo na primeira linha, seu endereço na segunda
+# e o CEP e telefone na terceira. 
+print()
+print(f'Nome: Everton Serpa\nEndereço: Rua dos Anjos Nº 1100\nCEP: 60371-300 Cel (55)85 9 8104-8509' )
+
+# Explicando o código
+# Linha 3, usei o print para inserir uma quebra de linha.
+# Linha 4, coloquei a resolução da atividade dentro de um unico print e useu o (\n) para quebrar a linha. 
+        
