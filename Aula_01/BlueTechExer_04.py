47aa3143850ca1abf395bd99f33813b8dc2be7b3
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-28 13:10:11 -0300

    exer_04

---
+++ Aula_01/BlueTechExer_04.py
@@ -0,0 +1,13 @@
+# 04 - Elabore um programa que recebe dois valores inteiros e mostra a soma desses valores
+print()
+var01 =  int(input("Digite um número: "))
+var02 = int(input("Digite o segundo número: ")) 
+soma = var01 + var02
+print(f'A soma do valor {var01} e {var02} é igual a: {soma}')
+
+# Explicando o código 
+# linha 2, o print foi usado para inserir um linha(no caso ele printa uma linha em branco)
+# linha 3, foi criado a variavel var01 para receber o valor inteiro informado pelo usuario
+# linha 4, foi criada a variavel var02 para receber o valor inteiro informado pelo usuario
+# linha 5, foi criado a variavel soma para calcular a soma entre os dois valores informado pelo usuario
+# linha 6, printei as informações solicitada pela questão
