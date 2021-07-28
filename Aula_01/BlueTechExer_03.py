dbfcbc4c764704ff249d2e3896982b58d38f8a78
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-28 12:53:41 -0300

    explicando o código

--- Aula_01/BlueTechExer_03.py
+++ Aula_01/BlueTechExer_03.py
@@ -3,3 +3,8 @@
 print()
 nome = input('Digite o seu nome: ')
 print(f'Olá {nome}! Sejá bem vindo ao fantástico mundo da programação.')
+
+# Explicando o código
+# Linha 3, foi usado um print para inserir uma quebra de linha
+# Linha 4, foi criada uma variavel que recebe o nome digitada pelo usuario
+# Linha 5, print com os dados solicitados na questão. 
