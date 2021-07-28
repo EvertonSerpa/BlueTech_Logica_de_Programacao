0528fcf29e76997f9c25b2333c3db890c5b89b47
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-28 12:49:05 -0300

    exer_03

---
+++ Aula_01/BlueTechExer_03.py
@@ -0,0 +1,5 @@
+# 03 - Elabore um programa que recebe o nome de uma pessoa do terminal e mostra a seguinte mensagem:
+# `Olá {nome}! Seja bem vindo ao fantástico mundo da programação`
+print()
+nome = input('Digite o seu nome: ')
+print(f'Olá {nome}! Sejá bem vindo ao fantástico mundo da programação.')
