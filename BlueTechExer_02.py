6b804b2bdf8cab841cbe1137aea741a192cf5ee7
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-28 11:59:59 -0300

    BlueTechExer_01_Primeiro_Programa

--- BlueTechExer_02.py
+++ BlueTechExer_02.py
@@ -3,7 +3,7 @@
 print()
 print(f'Nome: Everton Serpa\nEndereço: Rua dos Anjos Nº 1100\nCEP: 60371-300 Cel (55)85 9 8104-8509' )
 
-# Explicando o código.
+# Explicando o código
 # Linha 3, usei o print para inserir uma quebra de linha.
 # Linha 4, coloquei a resolução da atividade dentro de um unico print e useu o (\n) para quebrar a linha. 
         
