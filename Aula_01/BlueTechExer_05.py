7ff3eec1e7df14101a593bc6792a40510465f733
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-28 14:47:48 -0300

    comentando o codigo

--- Aula_01/BlueTechExer_05.py
+++ Aula_01/BlueTechExer_05.py
@@ -7,3 +7,8 @@ nota04 = float(input('Digite a quarta nota do aluno '))
 media = (nota01 + nota02 + nota03 + nota04) / 4
 print()
 print(f'A primeira nota: {nota01}\nA segunda nota: {nota02}\nA terceira nota: {nota03}\nA quarta nota: {nota04}\nE A média: {media}')
+
+# Explicando o código
+# Foi criado 4 variaveis para notas, onde cada variavel recebe um valor de uma nota especifica.
+# a variavel média calcula a média final do aluno. 
+# o print formatado apresesnta as informações das notas e média. \n = quebra de linha.
